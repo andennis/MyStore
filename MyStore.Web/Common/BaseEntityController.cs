ed1d7724bd8659c2ae8351a2a1903778621268c0
denisan <d.andreyeu@godeltech.com>
2016-02-25 21:57:59 +0300

    Simplify BaseEntityController

--- MyStore.Web/Common/BaseEntityController.cs
+++ MyStore.Web/Common/BaseEntityController.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Helpers;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
-using System.Web.UI.WebControls;
 using Microsoft.Practices.Unity;
 using AutoMapper;
 using Common.BL;
@@ -42,19 +36,11 @@ namespace MyStore.Web.Common
         }
 
         [HttpGet]
-        public virtual ActionResult Create()
-        {
-            return Create(x => { });
-        }
-
-        protected ActionResult Create(Action<TEntityViewModel> prepareModelAction)
+        public ActionResult Create()
         {
             var model = new TEntityViewModel();
             SetDefaultReturnUrl(model);
 
-            prepareModelAction?.Invoke(model);
-
-            PrepareModelToCreateView(model);
             return CreateView(model);
         }
 
@@ -64,44 +50,27 @@ namespace MyStore.Web.Common
         {
             if (ModelState.IsValid)
             {
-                DoCreate(model);
-
-                if (Request.IsAjaxRequest())
-                    return JsonEx();
-
+                TEntity entity = Mapper.Map<TEntityViewModel, TEntity>(model);
+                _service.Create(entity);
                 return RedirectTo(model);
             }
 
-            PrepareModelToCreateView(model);
             return CreateView(model);
         }
 
-        protected virtual void DoCreate(TEntityViewModel model)
-        {
-            TEntity entity = Mapper.Map<TEntityViewModel, TEntity>(model);
-            _service.Create(entity);
-        }
         protected ActionResult CreateView(object model)
         {
-            if (Request.IsAjaxRequest())
-                return PartialView("_Create", model);
-
             return View("Create", model);
         }
 
         [HttpGet]
         public virtual ActionResult Edit(int id)
         {
-            TEntityViewModel model = GetViewModel(id);
-            SetDefaultReturnUrl(model);
-            PrepareModelToEditView(model);
-            return EditView(model);
-        }
+            TEntity entity = _service.Get(id);
+            TEntityViewModel model = Mapper.Map<TEntity, TEntityViewModel>(entity);
 
-        protected virtual TEntityViewModel GetViewModel(int entityId)
-        {
-            TEntity entity = _service.Get(entityId);
-            return Mapper.Map<TEntity, TEntityViewModel>(entity);
+            SetDefaultReturnUrl(model);
+            return EditView(model);
         }
 
         [HttpPost]
@@ -110,38 +79,19 @@ namespace MyStore.Web.Common
         {
             if (ModelState.IsValid)
             {
-                DoUpdate(model);
-
-                if (Request.IsAjaxRequest())
-                    return JsonEx();
+                TEntity entity = _service.Get(model.EntityId);
+                entity = Mapper.Map<TEntityViewModel, TEntity>(model, entity);
+                _service.Update(entity);
 
                 return RedirectTo(model);
             }
 
-            PrepareModelToEditView(model);
             return EditView(model);
         }
 
-        protected virtual void DoUpdate(TEntityViewModel model)
-        {
-            TEntity entity = _service.Get(model.EntityId);
-            entity = Mapper.Map<TEntityViewModel, TEntity>(model, entity);
-            _service.Update(entity);
-        }
         private ActionResult EditView(object model)
         {
-            if (Request.IsAjaxRequest())
-                return PartialView("_Edit", model);
-
             return View(model);
-        }
-
-        [AjaxOnly]
-        public virtual ActionResult Get(int id)
-        {
-            TEntity entity = _service.Get(id);
-            TEntityViewModel model = Mapper.Map<TEntity, TEntityViewModel>(entity);
-            return JsonEx(model, JsonRequestBehavior.AllowGet);
         }
 
         [AjaxOnly]
@@ -158,18 +108,6 @@ namespace MyStore.Web.Common
         {
             _service.Delete(id);
             return JsonEx();
-        }
-
-        protected virtual void PrepareModelToCreateView(TEntityViewModel model)
-        {
-        }
-        protected virtual void PrepareModelToEditView(TEntityViewModel model)
-        {
-        }
-
-        protected void SetFormAttributes(object htmlAttributes)
-        {
-            ViewBag.HtmlFormAttributes = htmlAttributes;
         }
 
         protected virtual void SetDefaultReturnUrl(TEntityViewModel model)
